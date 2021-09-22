f6eca4ef7ade060c57d589ab9e4b884da816f818
Nill-Kigers <90038624+Nill-Kigers@users.noreply.github.com>
2021-09-22 13:11:51 +0300

    Add files via upload

---
+++ C#/PasswordCheckerTests.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class PasswordCheckerTests
+    {
+        [TestMethod()]
+        public void Check_8Symbols_ReturnsTrue()
+        {
+            //Arrange
+            string password = "ASqw12$$";
+            bool expected = true;
+            //Act
+            bool actual =
+        PasswordChecker.validatePassword(password);
+            //Assert
+            Assert.AreEqual(expected, actual);
+            
+        }
+        [TestMethod()]
+        public void Check_4Symbols_ReturnsFalse()
+        {
+            //Arrange
+            string password = "Aq1$";
+            bool expected = false;
+            //Act
+            bool actual =
+        PasswordChecker.validatePassword(password);
+            //Assert
+            Assert.IsFalse(actual);
+
+        }
+        [TestMethod()]
+        public void Check_20Symbols_ReturnsFalse()
+        {
+            //Arrange
+            string password = "Aq1$oidrjwpsejustpstupsa3opstupstipstei";
+            bool expected = false;
+            //Act
+            bool actual =
+        PasswordChecker.validatePassword(password);
+            //Assert
+            Assert.IsFalse(actual);
+
+        }
+        [TestMethod()]
+        public void Check_notDigit_ReturnsFalse()
+        {
+            //Arrange
+            string password = "Awerfsfrgdg";
+            bool expected = false;
+            //Act
+            bool actual =
+        PasswordChecker.validatePassword(password);
+            //Assert
+            Assert.IsFalse(actual);
+
+        }
+        [TestMethod()]
+        public void Check_NoSymbols_ReturnsFalse()
+        {
+            //Arrange
+            string password = "11223455";
+            bool expected = false;
+            //Act
+            bool actual =
+        PasswordChecker.validatePassword(password);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        [TestMethod()]
+        public void Check_NotUpperRegister_ReturnsFalse()
+        {
+            //Arrange
+            string password = "asgftrh12edg34!";
+            bool expected = false;
+            //Act
+            bool actual =
+        PasswordChecker.validatePassword(password);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        [TestMethod()]
+        public void Check_Digit_ReturnsTrue()
+        {
+            //Arrange
+            string password = "GGhs!1";
+            bool expected = false;
+            //Act
+            bool actual =
+        PasswordChecker.validatePassword(password);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        [TestMethod()]
+        public void Check_Nodigit_ReturnsTrue()
+        {
+            //Arrange
+            string password = "Aks!si";
+            bool expected = false;
+            //Act
+            bool actual =
+        PasswordChecker.validatePassword(password);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        [TestMethod()]
+        public void Check_SpecialSymbols_ReturnsTrue()
+        {
+            //Arrange
+            string password = "!aksaAD";
+            bool expected = false;
+            //Act
+            bool actual =
+        PasswordChecker.validatePassword(password);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        [TestMethod()]
+        public void Check_LowercaseLetters_ReturnsTrue()
+        {
+            //Arrange
+            string password = "Kfd124!";
+            bool expected = false;
+            //Act
+            bool actual =
+        PasswordChecker.validatePassword(password);
+            //Assert
+            Assert.IsFalse(actual);
+
+        }
+        [TestMethod()]
+        public void Check_CorrectPassword_ReturnsTrue()
+        {
+            //Arrange
+            string password = "Alquo10!";
+            bool expected = false;
+            //Act
+            bool actual =
+        PasswordChecker.validatePassword(password);
+            //Assert
+            Assert.IsFalse(actual);
+
+        }
+
+    }
+}
---
+++ C#/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class PasswordChecker
+    {
+        public static bool validatePassword(string password)
+        {
+            if (password.Length < 8 || password.Length > 20)
+                return false;
+            if (!password.Any(Char.IsLower))
+                return false;
+            if (!password.Any(Char.IsUpper))
+                return false;
+            if (!password.Any(Char.IsDigit))
+                return false;
+            if (password.Intersect("#$%^&_").Count()==0)
+                return false;
+            return true;
+        }
+
+        public static void Main(string[] args)
+        {
+            bool arr = PasswordChecker.validatePassword("1)Kghbtn");
+
+            Console.WriteLine("res=" + arr);
+            Console.ReadKey();
+        }
+    }
+}
