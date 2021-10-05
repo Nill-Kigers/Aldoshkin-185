4cf5408cfa314be7888a2ff2acb770d3c02b69eb
Nill-Kigers <90038624+Nill-Kigers@users.noreply.github.com>
2021-10-05 12:04:38 +0300

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
+	setlocale(LC_ALL, "Russian"); // задает языковой стандарт
 	Mat img;
 	img = imread("3.jpg", 1);
 	namedWindow("okno", WINDOW_AUTOSIZE);
@@ -18,15 +18,15 @@ int main()
 	Mat canny_output;
 	cvtColor(img, src_gray, COLOR_RGB2BGR);
 	blur(src_gray, src_gray, Size(3, 3));
-	double lower_thresh_val = 100, high_thresh_val = 300;
-	Canny(src_gray, canny_output, lower_thresh_val, high_thresh_val, 3);
-	namedWindow("����� �����������", WINDOW_AUTOSIZE);
-	imshow("����� �����������", canny_output);
+	double lower_thresh_val = 100, high_thresh_val = 300; //Создание бинарной системы цвета (255 - белый)
+	Canny(src_gray, canny_output, lower_thresh_val, high_thresh_val, 3); //оператор обнаружения границ изображения
+	namedWindow("Ñåðîå èçîáðàæåíèå", WINDOW_AUTOSIZE);
+	imshow("Ñåðîå èçîáðàæåíèå", canny_output);
 	imwrite("canny_output.jpeg", canny_output);
 	RNG rng(12345);
 	vector<vector<Point>>contours;
 	vector<Vec4i>hierarchy;
-	findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+	findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));//нахождение контуров
 	vector<Moments>mu(contours.size());
 	for (int i = 0; i < contours.size(); i++)
 {
@@ -38,7 +38,7 @@ mu[i] = moments(contours[i], false);
 		mc[i] = Point2f(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
 	}
 	for (int i = 0;i < contours.size(); i++) {
-		printf("������ � %d:���� ���� - x = %.2f y = %.2f; ����� - %.2f \n", i, mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00, arcLength(contours[i], true));
+		printf("êîíòóð ¹ %d:öåíò ìàññ - x = %.2f y = %.2f; äëèíà - %.2f \n", i, mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00, arcLength(contours[i], true));
 	}
 	
 	Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
@@ -48,9 +48,9 @@ mu[i] = moments(contours[i], false);
 		drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
 		circle(drawing, mc[i], 4, color, -1, 5, 0);
 	}
-	namedWindow("�������", WINDOW_AUTOSIZE);
-	imshow("�������", drawing);
-	imwrite("�������.jpeg", drawing);
+	namedWindow("êîíòóðû", WINDOW_AUTOSIZE);
+	imshow("êîíòóðû", drawing);
+	imwrite("êîíòóðû.jpeg", drawing);
 	waitKey(0);
 	system("pause");
 	return(0);
